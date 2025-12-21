aa0d46a791f08b9d662f3a4e1f3e468f45e5aff5
Gyu1234 <jfhffr@gmail.com>
2025-12-21 11:22:28 +0900

    First_Setting

---
+++ src/main/java/com/jravel/japan_web/JapanWebApplication.java
@@ -0,0 +1,13 @@
+package com.jravel.japan_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapanWebApplication.class, args);
+    }
+
+}
