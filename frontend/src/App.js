2ffcd6c42cdbb5f2cdda5162008711c0fce7c9ec
RKneco <sinrjs4114@naver.com>
2026-01-23 17:03:19 +0900

    메인페이지 일본의 문화 연결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,19 @@ import MainPage from './pages/main/MainPage';
 import ListPage from './pages/list_array/ListPage'; // 리스트 페이지 추가
 import DetailPage from './pages/detail/DetailPage';
 // main 카드9개 임포트
-import KantoPage from './pages/list_array/ListPages/KantoPage';
-import TohokuPage from './pages/list_array/ListPages/TohokuPage';
-import HokkaidoPage from './pages/list_array/ListPages/HokkaidoPage';
-import ChugokuPage from './pages/list_array/ListPages/ChugokuPage';
-import KansaiPage from './pages/list_array/ListPages/KansaiPage';
-import KyushuPage from './pages/list_array/ListPages/KyushuPage';
-import ShikokuPage from './pages/list_array/ListPages/ShikokuPage';
-import OkinawaPage from './pages/list_array/ListPages/OkinawaPage';
-import ChubuPage from './pages/list_array/ListPages/ChubuPage';
+import KantoPage from './pages/list_array/components/KantoPage';
+import TohokuPage from './pages/list_array/components/TohokuPage';
+import HokkaidoPage from './pages/list_array/components/HokkaidoPage';
+import ChugokuPage from './pages/list_array/components/ChugokuPage';
+import KansaiPage from './pages/list_array/components/KansaiPage';
+import KyushuPage from './pages/list_array/components/KyushuPage';
+import ShikokuPage from './pages/list_array/components/ShikokuPage';
+import OkinawaPage from './pages/list_array/components/OkinawaPage';
+import ChubuPage from './pages/list_array/components/ChubuPage';
+import MatsuriPage from './pages/detail/components/MatsuriPage';
+import TraditionPage from './pages/detail/components/TraditionPage';
+import ArtsPage from './pages/detail/components/ArtsPage';
+import BuildPage from './pages/detail/components/BuildPage';
 // 스타일 임포트
 import './styles/layout/footer.css';
 import './styles/layout/header.css';
@@ -39,6 +43,12 @@ function App() {
           <Route path="/shikoku" element={<ShikokuPage />} />
           <Route path="/okinawa" element={<OkinawaPage />} />
           <Route path="/chubu" element={<ChubuPage />} />
+          <Route path="/matsuri" element={<MatsuriPage />} />
+          <Route path="/tradition" element={<TraditionPage />} />
+          <Route path="/arts" element={<ArtsPage />} />
+          <Route path="/build" element={<BuildPage />} />
+          <Route path=":anything/cat" element={<DetailPage />} />
+          <Route path=":anything/dog" element={<DetailPage />} />
           {/* 나중에 검색 페이지를 만드시면 아래 줄의 주석을 푸세요 */}
           {/* <Route path="/destinations" element={<SearchPage />} /> */}
         </Routes>
--- frontend/src/pages/main/components/CultureSection.js
+++ frontend/src/pages/main/components/CultureSection.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const CultureSection = () => {
     const cultures = [
-        { id: 'festivals', name: '축제', img: '/images/mainpage/culture/matsuri.jpg', desc: '일본의 다채로운 축제들' },
-        { id: 'traditions', name: '전통', img: '/images/mainpage/culture/tradition.jpg', desc: '오랜 역사와 전통 문화' },
-        { id: 'arts', name: '예술', img: '/images/mainpage/culture/arts.jpg', desc: '독특한 일본 예술 세계' },
-        { id: 'architecture', name: '건축', img: '/images/mainpage/culture/build.jpg', desc: '현대와 전통이 어우러진 건축물' },   
+        { id: 'festivals', name: '축제', slug: 'matsuri', img: '/images/mainpage/culture/matsuri.jpg', desc: '일본의 다채로운 축제들' },
+        { id: 'traditions', name: '전통', slug: 'tradition', img: '/images/mainpage/culture/tradition.jpg', desc: '오랜 역사와 전통 문화' },
+        { id: 'arts', name: '예술', slug: 'arts', img: '/images/mainpage/culture/arts.jpg', desc: '독특한 일본 예술 세계' },
+        { id: 'architecture', name: '건축', slug: 'build', img: '/images/mainpage/culture/build.jpg', desc: '현대와 전통이 어우러진 건축물' },   
     ];
 
     return (
         <section className="culture-section">
             <h2 className="section-title">일본의 문화</h2>
             <div className="item-list">
-                {cultures.map(culture => (
-                    <div key={culture.id} className="item" style={{ backgroundImage: `url(${culture.img})` }}>
-                        <div className="item-desc">
-                            <h3>{culture.name}</h3>
-                            <p>{culture.desc}</p>
+                {cultures.map((culture) => (
+                    <Link to={`/${culture.slug}`} key={culture.id}>
+                        <div key={culture.id} className="item" style={{ backgroundImage: `url(${culture.img})` }}>
+                            <div className="item-desc">
+                                <h3>{culture.name}</h3>
+                                <p>{culture.desc}</p>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </section>
